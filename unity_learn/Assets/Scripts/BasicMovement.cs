5dda925af4ce35a5861df9ab2d7bacee5c698a2a
Hugo J <hjbello.wk@gmail.com>
2021-01-05 18:16:05 +0100

    initial commit

---
+++ unity_learn/Assets/Scripts/BasicMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicMovement : MonoBehaviour
+{
+    public bool isDragging;
+    public bool enters;
+
+    private void OnMouseUp() {
+        isDragging = false;
+
+    }
+
+    private void OnMouseDown() {
+        isDragging = true;
+    }
+
+    void Update()
+    {
+        if (isDragging) {
+            Vector2 position = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            transform.Translate(position);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        enters = true;
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+                enters = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        enters = false;
+    }
+
+}
---
+++ unity_learn/Assets/Scripts/DragArea.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragArea : MonoBehaviour
+{
+        public Animator animator;
+        public bool enters;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        animator.SetBool("IsActive",true);
+        enters = true;
+    }
+    private void OnTriggerStay2D(Collider2D other) {
+                enters = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        animator.SetBool("IsActive",false);
+        enters = false;
+    }
+}
